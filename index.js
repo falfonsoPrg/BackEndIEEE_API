5ed222649ee570c0bc4b4e44f3c3b15741dc8fd0
falfonsoPrg <falfonso@unbosque.edu.co>
2021-02-06 09:29:00 -0500

    First commit of Development

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 4000
+dotenv.config();
+
+//Cors configuration
+const config = {
+    application: {
+        cors: {
+            server: [{
+                origin: "*",
+                credentials: true
+            }]
+        }
+    }
+}
+app.use(cors(
+    config.application.cors.server
+));
+
+//Middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static('public'));
+
+//Fisrt route
+app.get('/',(req,res)=>{
+    res.send("This project is the rest API of the student branch of University El Bosque <a href='/swagger'>Swagger</a>")
+})
+
+//Initialize the server
+app.listen(port,() => {
+    console.log('Server en el puerto ' + port)
+}) 
--- server/build/controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexController = void 0;
-class IndexController {
-    index(req, res) {
-        res.json({ text: 'This is the API for the IEEE Application' });
-    }
-}
-exports.indexController = new IndexController();
